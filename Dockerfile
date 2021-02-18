8e04ee2eaf07e02c004ffb070b6f68cb266b8005
Your Name <you@example.com>
2021-02-18 04:58:58 +0100

    bitcoinlibtest

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ WORKDIR /app
 RUN apt-get update && apt-get install -y postgresql postgresql-contrib default-mysql-server libpq-dev default-libmysqlclient-dev 
 RUN pip install -r requirements.txt
 EXPOSE 8080
-CMD [ "python", "./app.py" ] 
+CMD [ "python", "-u", "./app.py" ] 
--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import time
+from bitcoinlib.services.bitcoind import BitcoindClient
+
+base_url = 'http://rpcuser:rpcpassword@bitcoin-full-node-service:8332'
+bdc = BitcoindClient(base_url=base_url)
+txid = '63522845d294ee9b0188ae5cac91bf389a0c3723f084ca1025e7d9cdfe481ce1'
+rt = bdc.getrawtransaction(txid)
+print("Raw: %s" % rt)
 
 while(True):
     print("looping...")
