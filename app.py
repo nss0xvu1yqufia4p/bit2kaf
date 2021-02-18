c388ebb7a2f675bd50c9efb6155626ae6caaf6c6
Your Name <you@example.com>
2021-02-18 05:09:35 +0100

    changed password

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import time
 from bitcoinlib.services.bitcoind import BitcoindClient
 
-base_url = 'http://rpcuser:rpcpassword@bitcoin-full-node-service:8332'
+base_url = 'http://rpcuser:lcjxjwzbvnkqwtcm@bitcoin-full-node-service:8332'
 bdc = BitcoindClient(base_url=base_url)
 txid = '63522845d294ee9b0188ae5cac91bf389a0c3723f084ca1025e7d9cdfe481ce1'
 rt = bdc.getrawtransaction(txid)
